85e8925aa1b252ac647ff9be5102f6f3c361574f
Aaron Shanks <aaronshanks3@gmail.com>
2023-01-16 21:49:14 -0600

    first commit

---
+++ index.py
@@ -0,0 +1,20 @@
+print(
+"Hello my fellow _nology bakers! Today We shall embark on a journey filled with cinnamon and sugar. We will be making cinnamon rolls! By the end of this journey you will have all the ingredients and skills to make the best cinnamon roll you ever had. Now, Lets get rolling"
+
+)
+
+name = input("What is your chef name?")
+print(f'Hello Chef {name}, lets get baking!')
+
+#Def ready_up as function and put if in it
+ready_up = input("Are you ready?")
+if ready_up == "yes":
+  print("Great, follow me into the kitchen!")
+if ready_up == "no":
+  print("Well that is too bad. You have no choice, it is my adventrue and you will follow my rules.")
+
+print("We will be needing flour, sugar, eggs, yeast, brown sugar, milk, butter, cream cheese, and of course, cinnamon. How much? That is for you to decide, just know i will be here to make sure you use the right amount.")
+
+
+# def start():
+#   ready_up = 
