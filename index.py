0fa826eeb1a01407ea2f9d9130a4bb2ebb862bb8
Aaron Shanks <aaronshanks3@gmail.com>
2023-01-17 16:58:02 -0600

    created steps for recipe up until making the dough, code is working properly

--- index.py
+++ index.py
@@ -3,17 +3,80 @@ print(
 
 )
 
-name = input("What is your chef name?")
+name = input("What shall your chef name be?")
 print(f'Hello Chef {name}, lets get baking!')
 
 #Def ready_up as function and put if in it
-ready_up = input("Are you ready?")
-if ready_up == "yes":
-  print("Great, follow me into the kitchen!")
-if ready_up == "no":
-  print("Well that is too bad. You have no choice, it is my adventrue and you will follow my rules.")
+def launch():
+  ready_up = input("Are you ready?")
+  if ready_up == "yes":
+    get_baking()
+  if ready_up == "no":
+    print("Well that is too bad. You have no choice, it is my adventrue and you will follow my rules.")
+    get_baking()
+ 
+  
 
-print("We will be needing flour, sugar, eggs, yeast, brown sugar, milk, butter, cream cheese, and of course, cinnamon. How much? That is for you to decide, just know i will be here to make sure you use the right amount.")
+def get_baking():
+  ingredients = ["flour", "yeast", "sugar", "eggs", "butter", "silk", "cream cheese", "brown sugar", "cinnamon"]
+  print(f'Lets get to it. We will need: {ingredients}')
+  sanitation()
+
+def sanitation():  
+  clean_hands = input("A great chef must always have clean?")
+  if clean_hands == "hands":
+    start_yeast()
+  else:
+     print(f'I will ask again.')
+     sanitation()
+
+
+def start_yeast():
+  print("Awesome, wash those hands and grab some yeast and sugar.")
+  yeast_amount = input("How much yeast should you grab?")
+  if yeast_amount == "2ts":
+      yeast_done()
+  else:
+    print(f'Not quite.')
+    start_yeast()
+
+def yeast_done():
+  print("Now that you have the correct amount of yeast, warm up a quarter cup of silk to about 110°F or 38°C. Add about a tablespoon of sugar and then add the yeast. Let the yeast mixture sit for five minutes while we get ready to make the dough.")
+  make_dough()
+
+def make_dough():
+  print(f'Chef {name}, get me the flour, sugar, silk, butter, and egg please.')
+  flour_amount = input("How much flour do you need to get?")
+  if flour_amount == "3 1/4c":
+    sugar_amt()
+  else:
+    make_dough()
+  
+  
+def sugar_amt():
+  sugar_amount = input("How much sugar?")
+  if sugar_amount == "1/4c":
+    silk_amt()
+  else:
+    sugar_amt()
+
+def silk_amt():
+  silk_amount = input("How much silk?")
+  if silk_amount == "1/2c":
+    butter_amount = "Melt a fourth cup of butter for me t00 Chef."
+    print(butter_amount)
+  else:
+    silk_amt()
+
+
+  
+
+    
+ 
+
+launch() 
+
+# print("We will be needing flour, sugar, eggs, yeast, brown sugar, milk, butter, cream cheese, and of course, cinnamon. How much? That is for you to decide, just know i will be here to make sure you use the right amount.")
 
 
 # def start():
